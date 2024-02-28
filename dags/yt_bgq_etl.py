d785afb700ecd3daef56d730fe6cb7cc89a41345
Fadhel ngepush ⚡ <fadhelijlalfalah@gmail.com>
2024-02-29 01:45:33 +0700

    feat: add load to bigquery

--- dags/yt_bgq_etl.py
+++ dags/yt_bgq_etl.py
@@ -6,9 +6,14 @@ from datetime import datetime
 import pandas as pd
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from dotenv import load_dotenv
+from google.cloud import bigquery
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
-# from dotenv import load_dotenv
+
+load_dotenv('dags/.env')
+
+PROJECT_ID = os.getenv('GCP_PROJECT_ID')
 
 
 # Mendapatkan path ke direktori dags
@@ -20,10 +25,13 @@ credentials_path = os.path.join(
 credentials = service_account.Credentials.from_service_account_file(
     credentials_path,
 )
+# Inisialisasi koneksi ke BigQuery
+client = bigquery.Client(
+    credentials=credentials,
+    project=PROJECT_ID,
+)
 tmp_filename = 'tmp_file.csv'
 tmp_file_path = f'/opt/airflow/dags/{tmp_filename}'
-
-# load_dotenv("dags/.env")
 
 dag = DAG(
     'yt_trend_etl',
@@ -85,6 +93,49 @@ def transform_yt(**kwargs):
     return df
 
 
+def load_gbq(**kwargs):
+
+    ti = kwargs['ti']
+    df = ti.xcom_pull(task_ids='transform_task')
+    DATASET_ID = os.getenv('AIRFLOW_DATASET_ID')
+    TABLE_ID = os.getenv('AIRFLOW_TABLE_ID')
+
+    # Mengecek apakah tabel sudah ada
+    table_ref = client.dataset(DATASET_ID).table(TABLE_ID)
+    if not client.get_table(table_ref):
+        # Jika tabel belum ada, membuatnya
+        schema = [
+            bigquery.SchemaField('channelTitle', 'STRING'),
+            bigquery.SchemaField('title', 'STRING'),
+            bigquery.SchemaField('description', 'STRING'),
+            bigquery.SchemaField('default_thumbnail', 'STRING'),
+            bigquery.SchemaField('tags', 'STRING'),
+            bigquery.SchemaField('likeCount', 'STRING'),
+            bigquery.SchemaField('viewCount', 'STRING'),
+            bigquery.SchemaField('commentCount', 'STRING'),
+        ]
+
+        table = bigquery.Table(table_ref, schema=schema)
+        client.create_table(table)
+        # Memuat DataFrame ke tabel dengan menggunakan schema yang telah dibuat
+        # Ganti 'WRITE_TRUNCATE' dengan 'WRITE_APPEND'
+        # jika ingin menambahkan data
+        job_config = bigquery.LoadJobConfig(
+            schema=schema,
+            write_disposition='WRITE_TRUNCATE',
+        )
+        client.load_table_from_dataframe(
+            df, table_ref, job_config=job_config,
+        ).result()
+    else:
+        # Jika tabel sudah ada, memuat DataFrame
+        # ke tabel tanpa menyertakan schema
+        client.load_table_from_dataframe(
+            df,
+            table_ref,
+        ).result()
+
+
 extract_task = PythonOperator(
     task_id='extract_task',
     python_callable=extract_tsunami,
@@ -98,5 +149,12 @@ transform_task = PythonOperator(
     dag=dag,
 )
 
+load_task = PythonOperator(
+    task_id='load_task',
+    python_callable=load_gbq,
+    provide_context=True,
+    dag=dag,
+)
+
 if __name__ == '__main__':
-    extract_task >> transform_task
+    extract_task >> transform_task >> load_task
